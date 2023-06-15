e2334d51d20a1d356e9ae6dc6a9b85b675a00eaa
joviah1 <joviahanyango12@gmail.com>
2023-06-15 09:25:23 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,6 @@
-#include<stdio.h>
-/**
- * main-Entry point
- * 
- * Return:Always 0(Success)
- */
-int main(void)
-{
-	print("with proper grammar,but the outcome is a piece of art,\n");
-	return (0);
-}
+#include <stdio.h>
 
+int main() {
+    printf("Art is the lie that enables us to realize the truth,\n")
+     return 0;
+}	    
