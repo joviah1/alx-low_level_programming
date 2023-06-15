435b100f61633768e78edd4cd10f1498b0ede58b
joviah1 <joviahanyango12@gmail.com>
2023-06-15 09:41:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
- * main - A program that prints a new line using puts 
+ * main-Entry point
  *
- * Return: 0 (Sucess)
+ * Return:Always 0(Sucess)
  */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0)
 }
 
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/**
 int main() {
     printf("Art is the lie that enables us to realize the truth,\n")
      return 0;
