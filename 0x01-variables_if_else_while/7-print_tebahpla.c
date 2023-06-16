8bd41bcd1bf8e90e006d50184ba21ea517dcabf7
joviah1 <joviahanyango12@gmail.com>
2023-06-16 13:45:59 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase letters in reverse
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
