8b1fa79f6b735f83cf1a790bdcde652fbb298d65
joviah1 <joviahanyango12@gmail.com>
2023-06-16 11:09:01 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(letters[i]);
+	}
+	putcha('\n');
+	return (0);
+}
+
