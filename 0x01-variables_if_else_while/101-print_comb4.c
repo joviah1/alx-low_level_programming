0d4685fa0fbd80d28d13eed530c07403c978ddff
joviah1 <joviahanyango12@gmail.com>
2023-06-17 00:49:35 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints all possible different combinations of three digits.
@@ -40,5 +41,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
 
