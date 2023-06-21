65d7674ae948e72a73433286e24d747c7c2c8045
joviah1 <joviahanyango12@gmail.com>
2023-06-21 01:59:03 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int fib1 = 1, fib2 = 2, next;
+	int i;
+
+	printf("%lu, %lu", fib1, fib2);
+
+	for (i = 2; i < 50; i++)
+	{
+		next = fib1 + fib2;
+		printf(", %lu", next);
+		fib1 = fib2;
+		fib2 = next;
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
