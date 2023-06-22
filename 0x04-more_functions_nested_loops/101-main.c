91b419e7b82210d0195de75524eceecfdea8bc32
joviah1 <joviahanyango12@gmail.com>
2023-06-22 14:01:00 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  int n;
+
+  n = 98;
+  printf("%d\n", n);
+  n = 402;
+  printf("%d\n", n);
+  n = 1024;
+  printf("%d\n", n);
+  n = 0;
+  printf("%d\n", n);
+  n = -98;
+  printf("%d\n", n);
+  return (0);
+}
+
