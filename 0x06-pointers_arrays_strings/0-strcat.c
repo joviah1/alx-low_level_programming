62c2f9fbbb9d3711daae79822d95a577ade02f76
joviah1 <joviahanyango12@gmail.com>
2023-06-29 08:24:30 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,22 +2,28 @@
 
 /**
  * *_strcat - concatenates two strings
- * @dest: char type string
- * @src: char type string
- * Description: Copy the string pointed to by pointer `src` to
- * the buffer pointed to by `dest`
- * Return: Pointer to `dest`
+ * @dest: input value
+ * @src: input value
+ * Return: void
  */
 
 char *_strcat(char *dest, char *src)
 {
 	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
+	int j;
+	
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dest[i] = src[i];
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
-
 	return (dest);
 }
