ddf89e3435be856306b13e86595fa570d57ec824
joviah1 <joviahanyango12@gmail.com>
2023-06-29 09:27:15 -0700

    strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13 cipher.
+ * @s: The input string.
+ *
+ * Return: Pointer to the encoded string.
+ */
+char *rot13(char *s)
+{
+  int i, j;
+  char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+  for (i = 0; s[i] != '\0'; i++)
+  {
+    for (j = 0; input[j] != '\0'; j++)
+    {
+      if (s[i] == input[j])
+      {
+        s[i] = output[j];
+        break;
+      }
+    }
+  }
+
+  return s;
+}
+
