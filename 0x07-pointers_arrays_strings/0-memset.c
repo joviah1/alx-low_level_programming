3a85fea18141972f623e78c0376d3f1c8cb0183d
joviah1 <joviahanyango12@gmail.com>
2023-07-03 08:20:06 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	 int i;
+
+	 for (i = 0; (unsigned int) i < n; ++i)
+	 {
+		 s[i] = b;
+	 }
+	 return (s);
+}
