b47f7fd166d2a9f7a26ed6c1e9b6205b1f28ed2d
joviah1 <joviahanyango12@gmail.com>
2023-07-07 09:25:29 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memeset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	        unsigne int i;
+
+		for (i = 0; i < n; ++i)
+			s[i] = b;
+		return (s);
+}
