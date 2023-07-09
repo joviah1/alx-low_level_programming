6e7671ea386341999f2838b2688d661bfc28e258
joviah1 <joviahanyango12@gmail.com>
2023-07-09 02:18:19 -0700

    100-change.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,29 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - multiplies two numbers
- * @argc: argument count
- * @argv: argument vector
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: Always 0
+ * Return: the int converted from the string
  */
-
-int main(int argc, char *argv[])
+int _atoi(char *s)
 {
-	    if (argc != 3)
-	    {
-		    printf("Error\n");
-		    return 1;
-	    }
-	    {
-		    int num1 = atoi(argv[1]);
-		    int num2 = atoi(argv[2]);
-		    int result = num1 * num2;
-		    printf("%d\n", result);
-	    }
-	    return (0);
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
 
 
