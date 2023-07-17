135ab6eeb2105e2cf464b7b4572159585b39b942
joviah1 <joviahanyango12@gmail.com>
2023-07-17 06:04:13 -0700

    02-main.c

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- * main - prints the name
+ * Description: Prints the name of the source file
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
 }
+
