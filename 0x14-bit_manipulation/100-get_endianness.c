b3c3a4ce2e00fbbdac1fe189a3ba341ca503cdff
joviah1 <joviahanyango12@gmail.com>
2023-08-03 14:13:17 -0700

    101-password

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -16,16 +16,16 @@ int get_endianness(void)
 }
 int main(void)
 {
-	int n;
+        int n;
 
-	n = get_endianness();
-	if (n != 0)
-	{
-		printf("Little Endian\n");
-	}
-	else
-	{
-		printf("Big Endian\n");
-	}
-	return (0);
+        n = get_endianness();
+        if (n != 0)
+        {
+                printf("Little Endian\n");
+        }
+        else
+        {
+                printf("Big Endian\n");
+        }
+        return (0);
 }
