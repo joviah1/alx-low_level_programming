39f751a42524e34a70c555171a33257e9b3a166a
joviah1 <joviahanyango12@gmail.com>
2023-08-07 05:36:12 -0700

    1-create_file.c

---
+++ 0x15-file_io/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes character c to stdout
+ * @c: character to be print
+ *
+ * Return: success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
