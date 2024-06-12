60607205794e751d6dbaf8014cf7f24f47af9479
joviah1 <joviahanyango12@gmail.com>
2024-06-12 04:15:23 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,12 @@
+#include main.h
+/**
+ *  read_textfile -  reads a text file and prints 
+ *  it to the POSIX standard output
+ *
+ *  filename: filename (string pointer)
+ *  letters: number of letters printed
+ *  Return: number of letters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
